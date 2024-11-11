04246f2d71e77689ce24b7b44100d06365b1e29c
GHJ5368 <jesantbar@alum.us.es>
2024-11-11 11:56:05 +0100

    modified:   src/nombres.py modified:   src/nombres_test.py

--- src/nombres.py
+++ src/nombres.py
@@ -34,32 +34,37 @@ def calcular_nombres(frecuencia_nombres,genero=None):
         elif nombre.genero == genero:
             filtrado.add(nombre.nombre)
 
-    return filtrado
+    return sorted(filtrado)
 
 def calcular_top_nombres_de_año(frecuencia_nombres, anyo:int, limite=10, genero=None):
     res = []
     
     for nombre in frecuencia_nombres:
-        
-        if len(res) >= limite:
-            break
-
+        # (nombre.genero == None or nombre.genero == genero) and nombre.anyo == anyo
         if nombre.anyo == anyo:
             if genero == None:
                 res.append((nombre.nombre,nombre.frecuencia))
             elif nombre.genero == genero:
                 res.append((nombre.nombre,nombre.frecuencia))
     
-    return sorted(res, lambda tupla:tupla[1])
+
+
+    return sorted(res, key = lambda tupla:tupla[1], reverse=True)[:limite]
         
 def calcular_nombres_ambos_generos(frecuencia_nombres):
     res = set()
     hombres = calcular_nombres(frecuencia_nombres, "Hombre")
     mujeres = calcular_nombres(frecuencia_nombres, "Mujer")
+    
+
+    '''
     for nombre in hombres:
         if nombre in mujeres:
             res.add(nombre)
-    
+    '''
+
+    res = set(hombres).intersection(set(mujeres))
+
     return res
 
 def calcular_nombres_compuestos(frecuencia_nombres,genero=None):
@@ -74,3 +79,50 @@ def calcular_nombres_compuestos(frecuencia_nombres,genero=None):
                 res.add(nombre)
     
     return res
+
+def calcular_frecuencia_media_nombre_años():
+    pass
+
+def calcular_nombre_mas_frecuente_año_genero():
+    pass
+
+def calcular_año_mas_frecuencia_nombre(frecuencia_nombres, nombre):
+    frecuencia = 0
+    anyo = 0000
+    '''
+    for n in frecuencia_nombres:
+        if n.nombre == nombre:
+            res.append( (n.anyo, n.frecuencia) )
+
+    # return sorted(res, key= lambda x:x[1], reverse=True)[0][0]
+    return max((res, key= lambda x:x[1])[0]
+    '''
+    
+    for frecuencia_nombre in frecuencia_nombres:
+        if frecuencia_nombre.nombre == nombre and frecuencia_nombre.frecuencia > frecuencia:
+            frecuencia = frecuencia_nombre.frecuencia
+            anyo = frecuencia_nombre.anyo
+    
+    return anyo
+
+def calcular_nombres_mas_frecuentes(frecuencia_nombres, genero, decada, num=5):
+    dict_nombres_por_frecuencia = dict()
+    ## lista = calcular_nombres(frecuencia_nombres, genero)
+    for n in frecuencia_nombres:
+        if n.genero== genero and decada <= n.anyo <= decada+9 :
+            if n.nombre in dict_nombres_por_frecuencia:
+                dict_nombres_por_frecuencia[n.nombre] += n.frecuencia
+            
+            else:
+                dict_nombres_por_frecuencia[n.nombre] = n.frecuencia
+    '''
+    dict_nombres = sorted(dict_nombres_por_frecuencia.items(), key= lambda x:x[1], reverse=True)[:num]
+    res=[]    
+    for n in dict_nombres:
+        res.append(n[0])
+
+    return res
+    '''
+
+    ordenados = sorted(dict_nombres_por_frecuencia, key= dict_nombres_por_frecuencia.get, reverse=True)
+    return ordenados[:num]
--- src/nombres_test.py
+++ src/nombres_test.py
@@ -20,6 +20,16 @@ def test_calcular_nombres_ambos_generos(frecuencia_nombres):
 def test_calcular_nombres_compuestos(frecuencia_nombres,genero=None):
     print(calcular_nombres_compuestos(frecuencia_nombres,genero))
 
+def test_calcular_año_mas_frecuencia_nombre(frecuencia_nombres, nombre):
+    print(calcular_año_mas_frecuencia_nombre(frecuencia_nombres, nombre))
+
+def test_calcular_nombres_mas_frecuentes(frecuencia_nombres, genero, decada, num):
+    res = calcular_nombres_mas_frecuentes(frecuencia_nombres, genero, decada, num)
+    print("TEST de 'calcular_nombres_mas_frecuentes'")
+    print(f"Los nombres mas frecuentes de la decada de {decada} son: {res}")
+
+    print("####################################################################################")
+
 
 if __name__ == "__main__":
     fichero = "LAB-Nombres/data/frecuencias_nombres.csv"
@@ -28,4 +38,7 @@ if __name__ == "__main__":
     #test_calcular_nombres(frecuencia_nombres,"Mujer")   
     #test_calcular_top_nombres_de_año(frecuencia_nombres, 2006, 5, "Hombre")
     #test_calcular_nombres_ambos_generos(frecuencia_nombres)
-    test_calcular_nombres_compuestos(frecuencia_nombres,"Hombre")
+    #test_calcular_nombres_compuestos(frecuencia_nombres,"Hombre")
+    #test_calcular_año_mas_frecuencia_nombre(frecuencia_nombres, "VERA")
+    test_calcular_nombres_mas_frecuentes(frecuencia_nombres, "Mujer", 2000,5)
+
